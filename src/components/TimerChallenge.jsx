a3cdaa8ab513cf1e8f7adea700c612b6c3f7db7d
Mayank Sethi <mayank@digibrain.co.in>
2024-07-09 14:57:46 +0530

    handle stop function

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 const TimerChallenge = ({ title, targetTime }) => {
+  const timer = useRef();
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
   const handleStart = () => {
-    setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
     setTimerStarted(true);
+  };
+
+  const handleStop = () => {
+    clearTimeout(timer.current);
+    setTimerStarted(false);
   };
 
   return (
@@ -19,7 +25,7 @@ const TimerChallenge = ({ title, targetTime }) => {
         {targetTime} second{targetTime > 1 ? 's' : ''}{' '}
       </p>
       <p>
-        <button onClick={handleStart}>
+        <button onClick={timerStarted ? handleStop : handleStart}>
           {timerStarted ? 'Stop' : 'Start'} Challenge
         </button>
       </p>
